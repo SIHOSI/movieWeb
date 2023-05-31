30d37f102f1eb3b383d489c5adb15352be78bc62
Siho <siho8808@gmail.com>
2023-05-31 21:03:47 +0900

    123

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
-const API_KEY = config.apikey;
+const API_KEY = config.apikey; //config.js에서 apikey 불러오기
 
 const API_POPULAR = 'https://api.themoviedb.org/3/movie/popular?';
 
 const API_SEARCH = 'https://api.themoviedb.org/3/search/movie?';
 
-const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer e2124fff88b3cf85df566d38b1f8ae8f',
-    },
-};
+const main = document.getElementById('main');
+// console.log(main);
+// const main2 = document.querySelector('main');
+// console.log(main2); // main과 main2는 동일한 값을 반환
+
+// const main3 = document.querySelectorAll('main');
+// console.log(main3); // Nodelist를 반환
+
+const homeButton = document.getElementById('homeButton');
+
+const form = document.getElementById('form');
+
+const search = document.getElementById('search');
+
+const mouse = main.addEventListener('mouseover', (event) => {
+    // console.log(event); // mouseevent 객체 반환
+    const target = event.target;
+    if (target.tagName === 'IMG' || target.className === 'overview') {
+        target.style.cursor = 'pointer';
+    }
+}); // 마우스를 카드 위에 올렸을때 마우스포인터를 바꿔준다
 
 const color = (vote) => {
     if (vote >= 8) return 'green';
     else if (vote >= 5) return 'orange';
     else return 'red';
-};
+}; // 평점에 따라 색상 변경
 
 const imageClick = (movieId) => {
     alert(`영화 ID: ${movieId}`);
-};
+}; // 이미지 클릭시 영화아이디 알림 / 이 기능 안쓰면 overview 클릭 이벤트 깔끔해짐
 
-const homeButton = document.getElementById('homeButton');
-
-const main = document.getElementById('main');
-
-const mouse = main.addEventListener('mouseover', (event) => {
-    const target = event.target;
-    if (target.tagName === 'IMG' || target.className === 'overview') {
-        target.style.cursor = 'pointer';
-    }
-});
-
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-
-const getMovies = (API, options) => {
-    fetch(API, options)
+const getMovies = (API) => {
+    fetch(API)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data.results);
+            // console.log(data.results); //array안에 객체 20개가 들어있다 각 객체는 영화의 정보를 가지고 있음
             showMovies(data.results);
         });
 };
 
 const showMovies = (data) => {
-    main.innerHTML = '';
+    main.innerHTML = ''; // main요소 초기화, html에 적어놓은 샘플 카드 삭제
 
     data.forEach((item) => {
-        console.log(item);
+        // console.log(item); //영화 정보 객체
         const { id, title, poster_path, vote_average, overview } = item;
         const movie = document.createElement('div');
-        movie.classList.add('movie');
+        movie.classList.add('movie'); // 각 영화 정보를 토대로 movie 변수 생성, div요소를 생성하고 classList를 통해 movie 클래스를 추가.
         movie.innerHTML = `<img
                                 src="${
                                     'https://image.tmdb.org/t/p/w500' +
@@ -71,27 +71,28 @@ const showMovies = (data) => {
 
                             <div class="overview" onclick="imageClick(${id})">
                                 <h3>${overview}</h3>
-                            </div>`;
+                            </div>`; // 영화 카드 내용 구성, 평점은 color함수를 사용하여 적절한 색상 클래스를 동적할당, toFixed(1)사용해서 소수점한자리 까지 표시
 
-        main.appendChild(movie);
+        main.appendChild(movie); // 생성된 영화 카드를 main 하위에 추가.
     });
 
     const movies = document.querySelectorAll('.movie');
-    console.log(movies);
+    // console.log(movies); // movie명을 가진 모든 div를 Nodelist형태로 movies에 저장.
 
     movies.forEach((item) => {
+        // console.log(item); // div class=movie 안에 있는 모든 요소들. 추가된 20개의 영화들의 html 양식
         item.addEventListener('click', () => {
-            item.classList.toggle('active');
+            item.classList.toggle('active'); // 클릭하면 active클래스를 추가, 이미 존재한다면 제거, overview의 내용을 활성/비활성 상태로 전환.
         });
     });
 };
 
-getMovies(API_POPULAR + 'api_key=' + API_KEY + '&language=ko');
+getMovies(API_POPULAR + 'api_key=' + API_KEY + '&language=ko'); // getmovies 실행.
 
 form.addEventListener('submit', (text) => {
     text.preventDefault();
-
-    const searchTerm = search.value;
+    // console.log(text); // SubmitEvent 객체 반환
+    const searchTerm = search.value; // 입력한 값을 searchTerm 에 저장.
 
     if (searchTerm) {
         getMovies(
@@ -103,8 +104,8 @@ form.addEventListener('submit', (text) => {
     } else {
         getMovies(API_POPULAR + 'api_key=' + API_KEY + '&language=ko');
     }
-});
+}); // 검색어를 포함한 영화를 가져와서 화면에 표시 / 내용 없이 엔터를 치면 초기 화면으로 돌아감
 
 homeButton.addEventListener('click', () => {
     getMovies(API_POPULAR + 'api_key=' + API_KEY + '&language=ko');
-});
+}); // 홈버튼 누르면 첫번째 페이지로 돌아감.
